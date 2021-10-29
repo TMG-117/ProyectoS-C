3427a57c68624d0805135a2b46c698c8b86e3646
RodrigoAVV <rodrigo.vidal@virginiogomez.cl>
2021-10-29 11:48:07 -0300

    Se mejora layout

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route,
-App\Http\Controllers\ProductoController;
+    App\Http\Controllers\ProductoController;
 
 Route::resource('productos', ProductoController::class);
+
 Route::get('/',function(){
-	return view ('home');
+	return view ('welcome');
 });
